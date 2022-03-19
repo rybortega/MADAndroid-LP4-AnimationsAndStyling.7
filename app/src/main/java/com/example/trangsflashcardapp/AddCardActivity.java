25adbfbea5780a97a310f3ec8aed661ffb7fccb3
trangttdang <trangttdang01@gmail.com>
2022-03-18 23:39:13 -0400

    updated for Lab 2

---
+++ app/src/main/java/com/example/trangsflashcardapp/AddCardActivity.java
@@ -0,0 +1,68 @@
+package com.example.trangsflashcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class AddCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+
+        findViewById(R.id.close_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddCardActivity.this, MainActivity.class);
+                AddCardActivity.this.startActivity(intent);
+                finish();
+
+            }
+        });
+
+        findViewById(R.id.save_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String question = ((EditText) findViewById(R.id.editQuestion)).getText().toString();
+                String answerRight = ((EditText) findViewById(R.id.editAnswerRight)).getText().toString();
+                String answerWrong1 = ((EditText) findViewById(R.id.editAnswerWrong1)).getText().toString();
+                String answerWrong2 = ((EditText) findViewById(R.id.editAnswerWrong2)).getText().toString();
+                if (question.equals("") || answerRight.equals("") || answerWrong1.equals("") || answerWrong2.equals("")){
+                    Toast toast = Toast.makeText(getApplicationContext(), "You need to enter both Question & Answer", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                    toast.show();
+                } else {
+                    Intent data = new Intent();// create a new Intent, this is where we will put our data
+                    data.putExtra("question", question); // puts one string into the Intent, with the key as 'string1'
+                    data.putExtra("answerRight", answerRight); // puts another string into the Intent, with the key as 'string2
+                    data.putExtra("answerWrong1", answerWrong1);
+                    data.putExtra("answerWrong2", answerWrong2);
+                    setResult(RESULT_OK, data); // set result code and bundle data for response
+                    finish(); // closes this activity and pass data to the original activity that launched this activity
+                }
+            }
+        });
+
+        String question = getIntent().getStringExtra("question");
+        String answerRight = getIntent().getStringExtra("answerRight");
+        String answerWrong1 = getIntent().getStringExtra("answerWrong1");
+        String answerWrong2 = getIntent().getStringExtra("answerWrong2");
+
+        ((TextView) findViewById(R.id.editQuestion)).setText(question);
+        ((TextView) findViewById(R.id.editAnswerRight)).setText(answerRight);
+        ((TextView) findViewById(R.id.editAnswerWrong1)).setText(answerWrong1);
+        ((TextView) findViewById(R.id.editAnswerWrong2)).setText(answerWrong2);
+    }
+
+
+}
--- app/src/main/java/com/example/trangsflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/trangsflashcardapp/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewParent;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.content.Intent;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView question = (TextView)findViewById(R.id.flashcard_question);
-        TextView answer = (TextView)findViewById(R.id.flashcard_answer);
+        TextView question = findViewById(R.id.flashcard_question);
+        TextView answer = findViewById(R.id.flashcard_answer);
         question.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,9 +34,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        TextView answer1 = (TextView)findViewById(R.id.flashcard_answer1);
-        TextView answer2 = (TextView)findViewById(R.id.flashcard_answer2);
-        TextView answer3 = (TextView)findViewById(R.id.flashcard_answer3);
+        TextView answer1 = findViewById(R.id.flashcard_answer1);
+        TextView answer2 = findViewById(R.id.flashcard_answer2);
+        TextView answer3 = findViewById(R.id.flashcard_answer3);
 
 
         answer1.setOnClickListener(new View.OnClickListener() {
@@ -71,17 +73,17 @@ public class MainActivity extends AppCompatActivity {
 
         findViewById(R.id.toggle_choices_visibility).setOnClickListener(new View.OnClickListener() {
             boolean isShowingAnswers = true;
+
             @Override
             public void onClick(View view) {
 
-                if(isShowingAnswers) {
+                if (isShowingAnswers) {
                     ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.ic_iconmonstr_eye_5);
                     answer1.setVisibility(View.INVISIBLE);
                     answer2.setVisibility(View.INVISIBLE);
                     answer3.setVisibility(View.INVISIBLE);
                     isShowingAnswers = false;
-                }
-                else{
+                } else {
                     ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.ic_iconmonstr_eye_8);
                     answer1.setVisibility(View.VISIBLE);
                     answer2.setVisibility(View.VISIBLE);
@@ -92,8 +94,58 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        findViewById(R.id.add_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                MainActivity.this.startActivityForResult(intent, 100);
+            }
+        });
+
+        findViewById(R.id.edit_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                String question = ((TextView) findViewById(R.id.flashcard_question)).getText().toString();
+                String answerRight = ((TextView) findViewById(R.id.flashcard_answer1)).getText().toString();
+                String answerWrong1 = ((TextView) findViewById(R.id.flashcard_answer2)).getText().toString();
+                String answerWrong2 = ((TextView) findViewById(R.id.flashcard_answer3)).getText().toString();
+                intent.putExtra("question", question);
+                intent.putExtra("answerRight", answerRight);
+                intent.putExtra("answerWrong1", answerWrong1);
+                intent.putExtra("answerWrong2", answerWrong2);
+                MainActivity.this.startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 || resultCode == RESULT_OK) { // this 100 needs to match the 100 we used when we called startActivityForResult!
+            String question = data.getExtras().getString("question"); // 'question' needs to match the key we used when we put the string in the Intent
+            String answerRight = data.getExtras().getString("answerRight");
+            String answerWrong1 = data.getExtras().getString("answerWrong1");
+            String answerWrong2 = data.getExtras().getString("answerWrong2");
+
+            TextView answer1 = findViewById(R.id.flashcard_answer1);
+            TextView answer2 = findViewById(R.id.flashcard_answer2);
+            TextView answer3 = findViewById(R.id.flashcard_answer3);
+
+            ((TextView) findViewById(R.id.flashcard_question)).setText(question);
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(answerRight);
+            answer1.setText(answerRight);
+            answer2.setText(answerWrong1);
+            answer3.setText(answerWrong2);
+            Snackbar.make(findViewById(R.id.flashcard_question),
+                    "Card successfully created",
+                    Snackbar.LENGTH_SHORT)
+                    .show();
 
 
-
+            answer1.setBackgroundColor(getResources().getColor(R.color.mint));
+            answer2.setBackgroundColor(getResources().getColor(R.color.mint));
+            answer3.setBackgroundColor(getResources().getColor(R.color.mint));
+        }
     }
 }
